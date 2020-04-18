c7ac38c7d38fbe3b3caa05c83b0ed5bb74e87a9b
yuan <aaa>
2020-04-18 18:57:53 +0800

    增加物资采购-袁现程

--- src/main/java/edu/bupt/slms/service/warehouse/WarehousePurchaseService.java
+++ src/main/java/edu/bupt/slms/service/warehouse/WarehousePurchaseService.java
@@ -1,4 +1,18 @@
 package edu.bupt.slms.service.warehouse;
 
+import edu.bupt.slms.bean.PurchaseList;
+import edu.bupt.slms.mapper.PurchaseListMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class WarehousePurchaseService {
+
+    @Autowired
+    PurchaseListMapper purchaseListMapper;
+
+    public int addPurchaseList(PurchaseList purchaseList){
+
+        return purchaseListMapper.insertPurchaseList(purchaseList);
+    }
 }
